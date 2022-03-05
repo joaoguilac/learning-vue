289f10778152d618f459e38b2550a3a1ca2afdac
João Guilherme <joguicosta@hotmail.com>
2022-03-05 00:51:04 -0300

    Aula 29 finalizada

--- make_your_burger/src/components/Dashboard.vue
+++ make_your_burger/src/components/Dashboard.vue
@@ -2,7 +2,7 @@
   <div id="burger-table">
     <div>
       <div id="burger-table-heading">
-        <div class="order id">#:</div>
+        <div class="order-id">#:</div>
         <div>Cliente:</div>
         <div>Pão:</div>
         <div>Carne:</div>
@@ -11,15 +11,16 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão de trigo</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+              {{ opcional }}
+            </li>
           </ul>
         </div>
         <div>
@@ -35,7 +36,29 @@
 
 <script>
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: []
+    }
+  },
+  methods: {
+    async getPedidos() {
+      const req = await fetch("http://localhost:3000/burgers");
+      const data = await req.json();
+
+      this.burgers = data;
+
+      console.log(this.burgers);
+
+      // resgatar os status
+    }
+  },
+  mounted() {
+    this.getPedidos();
+  }
 }
 </script>
 
@@ -58,15 +81,15 @@ export default {
     border-bottom: 3px solid #333;
   }
 
-  #burger-table-heading div,
-  .burger-table-row div {
-    width: 19%;
-  }
-
   .burger-table-row {
     width: 100%;
     padding: 12px;
     border-bottom: 1px solid #CCC;
+  }
+
+  #burger-table-heading div,
+  .burger-table-row div {
+    width: 19%;
   }
 
   #burger-table-heading .order-id,
